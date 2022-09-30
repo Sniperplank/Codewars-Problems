e81228a6fca30c8ff5d1ebc6857e3b399c683932
Eilaf Aljundi <97557078+Sniperplank@users.noreply.github.com>
2022-09-29 23:10:45 -0400

    Create SevenAte9.py

---
+++ SevenAte9.py
@@ -0,0 +1,17 @@
+# Probelm Description
+"""
+Write a function that removes every lone 9 that is inbetween 7s.
+"""
+
+# Output Examples
+"""
+"79712312" --> "7712312"
+"79797"    --> "777"
+"""
+
+# Problem Solution
+
+def seven_ate9(s):
+    while '797' in s:
+        s = s.replace('797', '77')
+    return s
