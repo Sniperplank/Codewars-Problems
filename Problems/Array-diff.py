a00b3db6af18dd1d9298ef75bd05184969424056
Eilaf Aljundi <97557078+Sniperplank@users.noreply.github.com>
2022-10-06 20:55:20 -0400

    Update Array-diff.py

--- Problems/Array-diff.py
+++ Problems/Array-diff.py
@@ -1,12 +1,16 @@
 # Probelm Description
 """
-
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in list b keeping their order.
 """
 
 # Output Examples
 """
-N/A
+array_diff([1,2],[1]) == [2]
+array_diff([1,2,2,2,3],[2]) == [1,3]
 """
 
 # Problem Solution
 
+def array_diff(a, b):
+    return list(filter(lambda x: x not in b, a))
